c7ab0a34e3f7bacfabbd33c6dca8285ab55062d7
kdepaula <42978472+kdepaula@users.noreply.github.com>
2019-04-02 14:26:48 -0400

    Add files via upload

--- CreateAccount.java
+++ CreateAccount.java
@@ -1,4 +1,16 @@
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.util.ArrayList;
 
-public class CreateAccount {
+import javax.swing.JPanel;
 
+public class CreateAccount extends JPanel
+{
+	
+	public CreateAccount(ArrayList <BankAccount> accL)
+	{
+		setLayout(new GridBagLayout());
+		GridBagConstraints gbc = new GridBagConstraints();
+	}
+	
 }
